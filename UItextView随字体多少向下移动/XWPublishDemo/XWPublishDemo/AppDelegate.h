5423d65dca76971d21ee853db6d0fc4a83b797af
710771595 <lilongjun@192.168.2.102>
2016-12-13 10:28:07 +0800

    UITextView
    这是一个TextView随字体逐渐变化的demo

---
+++ UItextView随字体多少向下移动/XWPublishDemo/XWPublishDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XWPublishDemo
+//
+//  Created by 李龙军 on 16/4/15.
+//  Copyright © 2016年 李龙军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UItextView随字体多少向下移动/XWPublishDemo/XWPublishDemo/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  XWPublishDemo
+//
+//  Created by 李龙军 on 16/4/15.
+//  Copyright © 2016年 李龙军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+
+
+@end
+
